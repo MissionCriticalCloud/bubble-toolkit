12ab85703d60cbd4a9724b0dbf5b3a51fab99ac3
Remi Bergsma <github@remi.nl>
2015-05-02 14:59:34 +0200

    script to register template in CloudStack

---
+++ add_os_templates.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cloudmonkey set table default
+TEMPOSID=$(cloudmonkey list ostypes keyword="Other PV (64-bit)" filter=id | grep ^id | awk {'print $3'})
+cloudmonkey register template displayText=Tiny format=VHD hypervisor=XenServer isextractable=true isfeatured=true ispublic=true isrouting=false name=Tiny osTypeId=$TEMPOSID passwordEnabled=true requireshvm=true zoneid=-1 url=http://dl.openvm.eu/cloudstack/macchinina/x86_64/macchinina-xen.vhd.bz2

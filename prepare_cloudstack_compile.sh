661d29b99305fcd94c8c149dd0645e7060fc66d5
Remi Bergsma <github@remi.nl>
2015-05-02 21:09:12 +0200

    script to prepare source for CloudStack compile

---
+++ prepare_cloudstack_compile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script to prepare source for Apache CloudStack compile
+
+# Get source
+mkdir -p /data/git/$HOSTNAME
+cd /data/git/$HOSTNAME
+if [ ! -d "cloudstack/.git" ]; then
+  echo "No git repo found, cloning Apache CloudStack"
+  git clone https://github.com/apache/cloudstack.git
+else
+  echo "Git Apache CloudStack repo already found"
+fi
+cd cloudstack
+
+# Check VHD-UTIL
+if [ ! -f "scripts/vm/hypervisor/xenserver/vhd-util" ]; then
+  echo "Fetching vhd-util.."
+  cd scripts/vm/hypervisor/xenserver
+  wget http://download.cloud.com.s3.amazonaws.com/tools/vhd-util
+  cd /data/git/$HOSTNAME/cloudstack
+fi
+
+# Set MVN compile options
+export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=512m -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n -Djava.net.preferIPv4Stack=true"
+pwd
+echo "Done."

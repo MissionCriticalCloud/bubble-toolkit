29a5522ddb3a7e0222618a4aaa1f82cf4b1d7f36
Remi Bergsma <github@remi.nl>
2015-05-02 21:10:49 +0200

    script to prepare CentOS7 for ACS mgt

---
+++ setup_cs_centos7.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Prepare CentOS7 bare box to compile CloudStack and run management server
+
+yum -y install maven tomcat6 mkisofs genisoimage gcc python MySQL-python openssh-clients wget git python-ecdsa bzip2 python-setuptools mariadb-server mariadb python-devel vim nfs-utils screen
+
+systemctl start mariadb.service
+systemctl enable mariadb.services
+systemctl stop firewalld.service
+systemctl disable firewalld.service
+
+mkdir -p /data
+mount -t nfs 192.168.22.1:/data /data
+
+mkdir -p /data/git
+cd /data/git
+cd /root
+
+wget https://raw.githubusercontent.com/remibergsma/dotfiles/master/.screenrc
+screen

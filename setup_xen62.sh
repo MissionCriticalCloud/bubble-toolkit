8496f8389552716f7bc6843bdb1a062ae91c64be
Remi Bergsma <github@remi.nl>
2015-05-06 15:17:37 +0200

    hack new uuids

--- setup_xen62.sh
+++ setup_xen62.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-# Fix hostname and uuid after deploying Xenserver 6.2 
-echo "sleep 5; xe host-param-set uuid=$(xe host-list params=uuid|awk {'print $5'}) name-label=\$HOSTNAME">>/etc/rc.local
+# Fix hostname and uuid after deploying Xenserver 6.2
+sed -i "/INSTALLATION_UUID/c\INSTALLATION_UUID='$(uuidgen)'" /etc/xensource-inventory
+sed -i "/CONTROL_DOMAIN_UUID/c\CONTROL_DOMAIN_UUID='$(uuidgen)'" /etc/xensource-inventory
+rm -f /var/xapi/state.db
+echo "sh /etc/rc.local.fix >> /etc/rc.local
+
+cat <<EOT >> /etc/rc.local.fix
+sleep 5
+xe host-param-set uuid=$(xe host-list params=uuid|awk {'print $5'}) name-label=\$HOSTNAME
+PIFUUID=$(xe pif-list params=uuid | awk {'print $5'})
+xe host-management-reconfigure pif-uuid=$PIFUUID
+xe pif-scan host-uuid=$(xe host-list params=uuid|awk {'print $5'})
+xe pif-reconfigure-ip uuid=$PIFUUID mode=dhcp
+xe host-management-reconfigure pif-uuid=$PIFUUID
+xe host-forget uuid=3794d17b-6721-442e-bc3b-53df3bbf407d --force
+rm /etc/rc.local.fix
+touch /etc/rc.local.fix
+EOT
+
 reboot

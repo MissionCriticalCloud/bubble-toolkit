c5b0f3ab2d7d93d98d50e7172e0c4b18ffbfa070
Ian Southam <isoutham@mct-isoutham.localdomain>
2015-05-07 12:47:39 +0000

    Quick and dirty add your key to a vm script TODO: sudoo config

---
+++ add_user.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#  Add your user account to a specific vm
+#
+# Usage ./add_user.sh -h <vmname>
+#
+RUSER=root
+RPASS=password
+TMPFILE=/tmp/${$}.sh
+while getopts "h:" opt; do
+  case $opt in
+    h)
+      HNAME=$OPTARG
+      echo Got here $HNAME
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+if [ -z "$HNAME" ]
+then
+	echo Usage $0 -h '<hostname>'
+	exit 1
+fi
+
+echo "KEY='" > $TMPFILE
+cat ${HOME}/.ssh/authorized_keys | sed -e '/^#/d' -e '/^\s*$/d' >> $TMPFILE
+echo "'" >> $TMPFILE
+
+cat << RSCRIPT >> ${TMPFILE}
+useradd ${LOGNAME}
+ADIR=/home/$LOGNAME/.ssh
+AFIL=\$ADIR/authorized_keys
+mkdir -p \$ADIR
+chown isoutham \$ADIR
+chmod 700 \$ADIR
+echo \$KEY > \$AFIL
+chown isoutham \$AFIL
+chmod 400 \$AFIL
+RSCRIPT
+
+sudo yum install -y sshpass
+sshpass -p $RPASS scp -o StrictHostKeyChecking=no $TMPFILE root@${HNAME}:adduser.sh
+if [ "$?" -ne "0" ]
+then
+	echo "Could not connect to $HNAME" >&2
+fi
+sshpass -p $RPASS ssh -o StrictHostKeyChecking=no root@${HNAME} "sh ./adduser.sh"

ff2e39ac0593c266627712673b5561d4c2f69334
Remi Bergsma <github@remi.nl>
2015-05-14 20:37:48 +0200

    get rid of centos5 audo-download

--- helper_scripts/cloudstack/build_run.sh
+++ helper_scripts/cloudstack/build_run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# We work from here
 cd /data/git/$HOSTNAME/cloudstack
+# Compile ACS
 mvn clean install -P developer,systemvm -DskipTests
+# Deploy DB
 mvn -P developer -pl developer -Ddeploydb
+# Get rid of CentOS 5 crap
+mysql -u cloud -pcloud cloud --exec "DELETE from vm_template where type=\"BUILTIN\";"
+# Run mgt
 mvn -pl :cloud-client-ui jetty:run

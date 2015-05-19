e620d36a242df9d623710dfb681134ae7660925b
Wilder Rodrigues <wrodrigues@schubergphilis.com>
2015-05-19 19:11:01 +0000

    The id of the BUILTIN template is 4, not 5 Also setting the hypervisor type as KVM, instead of XenServer

--- helper_scripts/cloudstack/build_run.sh
+++ helper_scripts/cloudstack/build_run.sh
@@ -9,7 +9,7 @@ mvn -P developer -pl developer -Ddeploydb
 # Configure the hostname properly - it doesn't exist if the deployeDB doesn't include devcloud
 mysql -u cloud -pcloud cloud --exec "INSERT INTO cloud.configuration (instance, name, value) VALUE('DEFAULT', 'host', '`hostname`') ON DUPLICATE KEY UPDATE value = '`hostname`';"
 # Get rid of CentOS 5 crap
-mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET unique_name='tiny Linux',name='tiny Linux',url='http://people.apache.org/~bhaisaab/vms/ttylinux_pv.vhd',checksum='046e134e642e6d344b34648223ba4bc1',display_text='tiny Linux' format='VHD' where id=5;"
+mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET unique_name='tiny Linux',name='tiny Linux',url='http://people.apache.org/~bhaisaab/vms/ttylinux_pv.vhd',checksum='046e134e642e6d344b34648223ba4bc1',display_text='tiny Linux' format='VHD', hypervisor_type='KVM'  where id=4;"
 
 # Run mgt
 mvn -pl :cloud-client-ui jetty:run

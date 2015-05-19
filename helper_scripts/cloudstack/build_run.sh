86d771673bbc83e7b011a2cfa498ffab46dd7b6b
Remi Bergsma <github@remi.nl>
2015-05-19 13:25:52 +0200

    dynamic hostname

--- helper_scripts/cloudstack/build_run.sh
+++ helper_scripts/cloudstack/build_run.sh
@@ -8,5 +8,6 @@ mvn clean install -P developer,systemvm -DskipTests
 mvn -P developer -pl developer -Ddeploydb
 # Get rid of CentOS 5 crap
 mysql -u cloud -pcloud cloud --exec "DELETE from vm_template where type=\"BUILTIN\";"
+mysql -u cloud -pcloud cloud --exec "UPDATE cloud.configuration SET value='`hostname`' WHERE name='host';"
 # Run mgt
 mvn -pl :cloud-client-ui jetty:run

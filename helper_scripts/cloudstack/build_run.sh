ecd5ca0756c4391813267d6d70899143c38d7157
Wilder Rodrigues <wrodrigues@schubergphilis.com>
2015-05-20 04:30:21 +0000

    Fix SQL statement   - Missing ,
    Add tiny linus for XenServer template as wel
      - Same VHD as the KVM onel

--- helper_scripts/cloudstack/build_run.sh
+++ helper_scripts/cloudstack/build_run.sh
@@ -9,7 +9,11 @@ mvn -P developer -pl developer -Ddeploydb
 # Configure the hostname properly - it doesn't exist if the deployeDB doesn't include devcloud
 mysql -u cloud -pcloud cloud --exec "INSERT INTO cloud.configuration (instance, name, value) VALUE('DEFAULT', 'host', '`hostname`') ON DUPLICATE KEY UPDATE value = '`hostname`';"
 # Get rid of CentOS 5 crap
-mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET unique_name='tiny Linux',name='tiny Linux',url='http://people.apache.org/~bhaisaab/vms/ttylinux_pv.vhd',checksum='046e134e642e6d344b34648223ba4bc1',display_text='tiny Linux' format='VHD', hypervisor_type='KVM'  where id=4;"
+mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET unique_name='tiny Linux KVM',name='tiny Linux',url='http://people.apache.org/~bhaisaab/vms/ttylinux_pv.vhd',checksum='046e134e642e6d344b34648223ba4bc1', \
+    display_text='tiny Linux KVM', format='VHD', hypervisor_type='KVM'  where id=4;"
+
+mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET unique_name='tiny Linux Xen',name='tiny Linux',url='http://people.apache.org/~bhaisaab/vms/ttylinux_pv.vhd',checksum='046e134e642e6d344b34648223ba4bc1', \
+    display_text='tiny Linux Xen', format='VHD', hypervisor_type='XernServer'  where id=5;"
 
 # Run mgt
 mvn -pl :cloud-client-ui jetty:run

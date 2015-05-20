b1e90605de6f158d5a961bb990801eff1ee38948
Wilder Rodrigues <wrodrigues@schubergphilis.com>
2015-05-20 06:55:40 +0000

    Fixing the management server host ip   - using the ip of the eth0 instead of the hostname

--- helper_scripts/cloudstack/build_run.sh
+++ helper_scripts/cloudstack/build_run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+host_ip=`ip addr | grep 'inet 192' | cut -d: -f2 | awk '{ print $2 }' | awk -F\/24 '{ print $1 }'`
+
 # We work from here
 cd /data/git/$HOSTNAME/cloudstack
 # Compile ACS
@@ -7,7 +9,7 @@ mvn clean install -P developer,systemvm -DskipTests
 # Deploy DB
 mvn -P developer -pl developer -Ddeploydb
 # Configure the hostname properly - it doesn't exist if the deployeDB doesn't include devcloud
-mysql -u cloud -pcloud cloud --exec "INSERT INTO cloud.configuration (instance, name, value) VALUE('DEFAULT', 'host', '`hostname`') ON DUPLICATE KEY UPDATE value = '`hostname`';"
+mysql -u cloud -pcloud cloud --exec "INSERT INTO cloud.configuration (instance, name, value) VALUE('DEFAULT', 'host', '$host_ip') ON DUPLICATE KEY UPDATE value = '$host_ip';"
 # Get rid of CentOS 5 crap
 mysql -u cloud -pcloud cloud --exec "UPDATE cloud.vm_template SET unique_name='tiny Linux KVM',name='tiny Linux',url='http://people.apache.org/~bhaisaab/vms/ttylinux_pv.vhd',checksum='046e134e642e6d344b34648223ba4bc1', \
     display_text='tiny Linux KVM', format='VHD', hypervisor_type='KVM'  where id=4;"

17a7a364a8510fd8ea976e04d914565198adcef6
Remi Bergsma <github@remi.nl>
2015-09-27 11:43:17 +0000

    script to checkout pr branch

---
+++ helper_scripts/cloudstack/checkout-pr.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script checks out a PR branch and then starts a management server with that code. 
+# Next, you can run Marvin to setup whatever you need to verify the PR.
+
+# Stop executing when we encounter errors
+set -e
+
+# Check if a pull request id was specified
+prId=$1
+if [ -z ${prId} ]; then
+  echo "No PR number specified. Quiting."
+  exit 1
+fi
+
+# Go the the source
+cd /data/git/${HOSTNAME}/cloudstack
+git checkout master
+
+# Get the PR
+git fetch origin pull/${prId}/head:pr/${prId}
+git checkout pr/${prId}

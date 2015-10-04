2cef1d6639ff82a7478e3c12649c62e2e38843a8
Remi Bergsma <github@remi.nl>
2015-10-04 16:15:21 +0200

    add sshpass

--- deploy/firstboot/centos7-kvm.sh
+++ deploy/firstboot/centos7-kvm.sh
@@ -14,6 +14,7 @@ yum -y install http://mirror.karneval.cz/pub/linux/fedora/epel/epel-release-late
 yum -y install qemu-kvm libvirt libvirt-python net-tools bridge-utils vconfig setroubleshoot virt-top virt-manager openssh-askpass wget vim
 yum -y install http://jenkins.buildacloud.org/job/package-centos7-master/lastSuccessfulBuild/artifact/dist/rpmbuild/RPMS/x86_64/cloudstack-common-4.6.0-SNAPSHOT.el7.centos.x86_64.rpm 
 yum -y install http://jenkins.buildacloud.org/job/package-centos7-master/lastSuccessfulBuild/artifact/dist/rpmbuild/RPMS/x86_64/cloudstack-agent-4.6.0-SNAPSHOT.el7.centos.x86_64.rpm
+yum --enablerepo=epel -y install sshpass
 
 # Enable rpbind for NFS
 systemctl enable rpcbind

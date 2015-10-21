701f127424d6db4a2ef00caee449acf5429a1d2f
Miguel Ferreira <miguelferreira@me.com>
2015-10-21 16:07:05 +0200

    Ping 5 times when waiting for machine to reboot

--- deploy/postboot/post_detect_reboot.sh
+++ deploy/postboot/post_detect_reboot.sh
@@ -7,7 +7,7 @@ echo "Note: ${NEWHOST}: Waiting for the VM to boot..."
 while ! ping -c1 ${NEWHOST} &>/dev/null; do :; done
 echo "Note: ${NEWHOST}: Installing and configuring"
 echo "Note: ${NEWHOST}: This will take some time. You may send this to the background."
-while ping -c1 ${NEWHOST} &>/dev/null; do :; done
+while ping -c5 ${NEWHOST} &>/dev/null; do :; done
 echo "Note: ${NEWHOST}: Rebooting"
 while ! ping -c1 ${NEWHOST} &>/dev/null; do :; done
 sleep 15

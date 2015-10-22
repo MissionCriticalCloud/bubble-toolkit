6a6a7896af7130f43fba6c624fd75913c010f616
Wilder Rodrigues <wrodrigues@schubergphilis.com>
2015-10-22 12:00:55 +0200

    Adding internal LB and ssvm tests

--- helper_scripts/cloudstack/run_marvin_router_tests.sh
+++ helper_scripts/cloudstack/run_marvin_router_tests.sh
@@ -18,7 +18,9 @@ component/test_vpc_redundant.py \
 component/test_routers_iptables_default_policy.py \
 component/test_routers_network_ops.py \
 component/test_vpc_router_nics.py \
-smoke/test_loadbalance.py
+smoke/test_loadbalance.py \
+smoke/test_internal_lb.py \
+smoke/test_ssvm.py
 
 echo "Running tests with required_hardware=false"
 nosetests --with-marvin --marvin-config=${marvinCfg} -s -a tags=advanced,required_hardware=false \

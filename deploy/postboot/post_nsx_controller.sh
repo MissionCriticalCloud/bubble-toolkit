db176a26134ace3e941a7384faad921e1dcc5011
Miguel Ferreira <miguelferreira@me.com>
2015-10-27 13:46:55 +0100

    Add role to deploy an extra NSX controller as part of a cluster

---
+++ deploy/postboot/post_nsx_controller.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+NSX_CONTROLLER=$1
+# We get this passed from the main script
+NEWHOST=$2
+
+NSX_CONTROLLER_IP=$(getent hosts ${NSX_CONTROLLER} | awk '{ print $1 }')
+
+SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q"
+
+sudo yum install -y -q sshpass
+sshpass -p 'admin' ssh ${SSH_OPTIONS} admin@${NEWHOST} join control-cluster ${NSX_CONTROLLER_IP}

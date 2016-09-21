aec7dda6670cac06a087738c65892e1a497d3fad
Boris Schrijver <bschrijver@schubergphilis.com>
2016-09-21 14:03:39 +0200

    Simplify post deploy script for NSX controller and service nodes.

--- deploy/default/postboot/post_nsx_controller.sh
+++ deploy/default/postboot/post_nsx_controller.sh
@@ -13,10 +13,3 @@ SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 echo "Cleaning controller node."
 echo 'yes' | sshpass -p 'admin' ssh ${SSH_OPTIONS} admin@${NSX_CONTROLLER_NODE} clear everything
 echo 'y' | sshpass -p 'admin' ssh ${SSH_OPTIONS} admin@${NSX_CONTROLLER_NODE} restart system
-
-while ping -c5 ${NSX_CONTROLLER_NODE} &>/dev/null; do :; done
-echo "Note: VM rebooting, waiting for it to come online."
-while ! ping -c1 ${NSX_CONTROLLER_NODE} &>/dev/null; do :; done
-
-echo "Note: VM ${NSX_CONTROLLER_NODE} is online!"
-ping -c1 ${NSX_CONTROLLER_NODE}

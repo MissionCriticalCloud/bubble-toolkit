9bca915703cc4944c680b946a4c3d05d7aaa5457
Boris Schrijver <bschrijver@schubergphilis.com>
2016-09-21 11:13:39 +0200

    Add reboot at the end of the postboot, nsx service node.

--- deploy/default/postboot/post_nsx_service_node.sh
+++ deploy/default/postboot/post_nsx_service_node.sh
@@ -12,3 +12,10 @@ SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
 echo "Cleaning service node."
 echo 'yes' | sshpass -p 'admin' ssh ${SSH_OPTIONS} admin@${NSX_SERVICE_NODE} clear everything
+echo 'y' | sshpass -p 'admin' ssh ${SSH_OPTIONS} admin@${NSX_SERVICE_NODE} restart system
+
+echo "Note: VM rebooting, waiting for it to come online."
+while ! ping -c1 ${NSX_SERVICE_NODE} &>/dev/null; do :; done
+
+echo "Note: VM ${NSX_SERVICE_NODE} is online!"
+ping -c1 ${NSX_SERVICE_NODE}

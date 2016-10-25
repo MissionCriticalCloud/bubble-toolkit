8293a2f980bc6559146ebbbad577019105dd5f40
Boris Schrijver <bschrijver@schubergphilis.com>
2016-10-26 00:39:49 +0200

    Wait for minikube to be available.

--- ci/ci-prepare-minikube.sh
+++ ci/ci-prepare-minikube.sh
@@ -11,3 +11,10 @@ until minikube_start "true"
 do
   say "minikube failed to start, retrying."
 done
+
+minikube_get_ip
+
+say "Waiting for kubernetes to be available."
+while ! nc -w 2 -v ${minikube_ip} 8443 </dev/null; do
+  sleep 1 # wait for 1/10 of the second before check again
+done

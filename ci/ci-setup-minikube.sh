3de60d6fea4047f6216e950f71f182e2de9b9384
Bob van den Heuvel <bvandenheuvel@schubergphilis.com>
2016-11-09 14:53:20 +0000

    Deploy a secure/trusted registry

--- ci/ci-setup-minikube.sh
+++ ci/ci-setup-minikube.sh
@@ -13,6 +13,31 @@ say "Running script: $0"
 
 minikube_get_ip
 
+function cosmic_docker_registry {
+    say "Generating certificates for registry"
+    mkdir -p /tmp/registry/certs
+    rm -f /tmp/registry/certs/*
+    # Generate self-signed certificate
+    openssl req -x509 -sha256 -nodes -newkey rsa:4096 -keyout /tmp/registry/certs/domain.key -out /tmp/registry/certs/domain.crt -days 365 -subj "/C=NL/ST=NH/L=AMS/O=SBP/OU=cosmic/CN=${MINIKUBE_HOST}" &> /dev/null
+    # Add certificate to local trust store
+    sudo cp /tmp/registry/certs/domain.crt /etc/pki/ca-trust/source/anchors/
+    sudo update-ca-trust
+    # Add certificate to the docker deamon (to trust)
+    minikube ssh "sudo mkdir -p /etc/docker/certs.d/${MINIKUBE_HOST}:30081"
+    cat /tmp/registry/certs/domain.crt | minikube ssh "sudo cat > ca.crt"
+    minikube ssh "sudo mv ca.crt /etc/docker/certs.d/${MINIKUBE_HOST}:30081/ca.crt"
+    minikube ssh "sudo /etc/init.d/docker restart"
+
+    say "Uploading certificates as secrets"
+    kubectl create secret generic registry-certs --from-file=/tmp/registry/certs/domain.crt --from-file=/tmp/registry/certs/domain.key --namespace=cosmic
+
+    say "Starting deployment: registry"
+    kubectl create -f /data/shared/deploy/cosmic/kubernetes/deployments/registry-deployment.yml
+
+    say "Starting service: registry"
+    kubectl create -f /data/shared/deploy/cosmic/kubernetes/services/registry-service.yml
+}
+
 function cosmic_usage_db {
     say "Setup Cosmic usage database"
     say "Starting deployment: mariadb"
@@ -37,11 +62,8 @@ kubectl create namespace cosmic
 # Setup usage db/container
 cosmic_usage_db
 
-say "Starting deployment: registry"
-kubectl create -f /data/shared/deploy/cosmic/kubernetes/deployments/registry-deployment.yml
-
-say "Starting service: registry"
-kubectl create -f /data/shared/deploy/cosmic/kubernetes/services/registry-service.yml
+# Setup docker registry with certificates
+cosmic_docker_registry
 
 say "Starting deployment: rabbitmq"
 kubectl create -f /data/shared/deploy/cosmic/kubernetes/deployments/rabbitmq-deployment.yml

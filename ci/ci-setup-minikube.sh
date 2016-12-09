e1295b8564e262f221d0ebc7c69127b7990c7469
Elias Gomes <egomes@schubergphilis.com>
2016-12-09 14:41:02 +0100

    Fixed broken regex during kubernetes setup.

--- ci/ci-setup-minikube.sh
+++ ci/ci-setup-minikube.sh
@@ -14,13 +14,12 @@ say "Running script: $0"
 minikube_get_ip
 
 say "Starting deployment: cosmic-config-server"
-cat /data/shared/deploy/cosmic/kubernetes/deployments/cosmic-config-server.yml | sed "s/image: .*cosmic-config-server/image: ${MINIKUBE_HOST}:30081\/missioncriticalcloud/cosmic-config-server/g" | kubectl create -f -
+cat /data/shared/deploy/cosmic/kubernetes/deployments/cosmic-config-server.yml | sed "s/image: .*cosmic-config-server/image: ${MINIKUBE_HOST}:30081\/missioncriticalcloud\/cosmic-config-server/g" | kubectl create -f -
 kubectl create -f /data/shared/deploy/cosmic/kubernetes/services/cosmic-config-server.yml
 
 say "Starting deployment: cosmic-metrics-collector"
-cat /data/shared/deploy/cosmic/kubernetes/deployments/cosmic-metrics-collector.yml | sed "s/image: .*cosmic-metrics-collector/image: ${MINIKUBE_HOST}:30081\/missioncriticalcloud/cosmic-metrics-collector/g" | kubectl create -f -
-kubectl create -f /data/shared/deploy/cosmic/kubernetes/services/cosmic-metrics-collector.yml
+cat /data/shared/deploy/cosmic/kubernetes/deployments/cosmic-metrics-collector.yml | sed "s/image: .*cosmic-metrics-collector/image: ${MINIKUBE_HOST}:30081\/missioncriticalcloud\/cosmic-metrics-collector/g" | kubectl create -f -
 
 say "Starting deployment: cosmic-usage-api"
-cat /data/shared/deploy/cosmic/kubernetes/deployments/cosmic-usage-api.yml | sed "s/image: .*cosmic-usage-api/image: ${MINIKUBE_HOST}:30081\/missioncriticalcloud/cosmic-usage-api/g" | kubectl create -f -
+cat /data/shared/deploy/cosmic/kubernetes/deployments/cosmic-usage-api.yml | sed "s/image: .*cosmic-usage-api/image: ${MINIKUBE_HOST}:30081\/missioncriticalcloud\/cosmic-usage-api/g" | kubectl create -f -
 kubectl create -f /data/shared/deploy/cosmic/kubernetes/services/cosmic-usage-api.yml

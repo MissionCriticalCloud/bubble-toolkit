67656f6dfc62b197f1c1fc89532e3e17e5509a5d
Boris Schrijver <bschrijver@schubergphilis.com>
2017-01-09 15:51:10 +0100

    Revert "Have the build use the latest released Marvin instead of snapshot"
    This reverts commit a7f8f6da275bf8a39284fc14be3f482e7ede9acb.

--- ci/ci-setup-infra.sh
+++ ci/ci-setup-infra.sh
@@ -441,7 +441,7 @@ say "Deploying Cosmic DB"
 deploy_cosmic_db ${cs1ip} ${cs1user} ${cs1pass}
 
 say "Installing Marvin"
-install_marvin "https://beta-nexus.mcc.schubergphilis.com/service/local/artifact/maven/redirect?r=releases&g=cloud.cosmic&a=cloud-marvin&v=RELEASE&p=tar.gz"
+install_marvin "https://beta-nexus.mcc.schubergphilis.com/service/local/artifact/maven/redirect?r=snapshots&g=cloud.cosmic&a=cloud-marvin&v=LATEST&p=tar.gz"
 
 say "Installing SystemVM templates"
 systemtemplate="/data/templates/cosmic-systemvm.qcow2"

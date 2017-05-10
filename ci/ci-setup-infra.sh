215345a2e0e52c4eba01843e5a485991f6c3e3c0
Bob <bvandenheuvel@schubergphilis.com>
2017-05-10 09:29:02 +0200

    Revert "Break on error"

--- ci/ci-setup-infra.sh
+++ ci/ci-setup-infra.sh
@@ -3,7 +3,7 @@
 scripts_dir=$(dirname $0)
 . ${scripts_dir}/../helper_scripts/cosmic/helperlib.sh
 
-set -e
+set -x
 
 function usage {
   printf "Usage: %s: -m marvin_config -c to inform we're configuring CloudStack instead of Cosmic\n" $(basename $0) >&2

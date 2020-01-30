33f0ff065028d3e2e63bfbe338d1d59e76dd695b
Alexander Verhaar <sanderv32@gmail.com>
2020-01-30 13:56:55 +0100

    Fixes to get everything working

--- ci/ci-cleanup.sh
+++ ci/ci-cleanup.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 # We need to be root to do the cleanup of all NFS files
+<<<<<<< HEAD
 sudo $(dirname $0)/ci-cleanup.py $*
+=======
+sudo $(dirname $0)/ci-cleanup.py $*
+>>>>>>> WIP

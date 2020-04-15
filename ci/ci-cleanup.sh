f9175209ad14211da78a301fff193a117c4d3a49
Alexander Verhaar <averhaar@schubergphilis.com>
2020-04-15 13:46:16 +0200

    Rebase against master

--- ci/ci-cleanup.sh
+++ ci/ci-cleanup.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 # We need to be root to do the cleanup of all NFS files
-<<<<<<< HEAD
 sudo $(dirname $0)/ci-cleanup.py $*
-=======
-sudo $(dirname $0)/ci-cleanup.py $*
->>>>>>> WIP
